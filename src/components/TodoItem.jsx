ff153450acccd16b84d6b9bdf7440d2787a7066f
lakshsidhu04 <cs23btech11031@iith.ac.in>
2024-02-17 16:43:49 +0530

    created the todo list

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { useTodo } from '../contexts/TodoContext';
+
+function TodoItem({ todo }) {
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
+    const { updateTodo, deleteTodo, toggleComplete } = useTodo()
+
+    const editTodo = () => {
+        updateTodo(todo.id, { ...todo, todo: todoMsg })
+        setIsTodoEditable(false)
+    }
+    const toggleCompleted = () => {
+        //console.log(todo.id);
+        toggleComplete(todo.id)
+    }
+
+    return (
+        <div
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#E9C46A]" : "bg-[#E76F51]"
+                }`}
+        >
+            {/* this entire code is checkbox */}
+            <div class="inline-flex items-center">
+                <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="customStyle">
+                    <input type="checkbox"
+                        className="before:content[''] peer relative h-8 w-8 cursor-pointer appearance-none rounded-full border border-gray-900/20 bg-gray-900/10 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:scale-105 hover:before:opacity-0"
+                        id="customStyle"
+                        checked={todo.completed}
+                        onChange={toggleCompleted}
+                    />
+                    <span
+                        class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                            stroke="currentColor" stroke-width="1">
+                            <path fill-rule="evenodd"
+                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </span>
+                </label>
+            </div>
+
+            {/* checkbox ends */}
+            <input
+                type="text"
+                className={`border outline-none w-full bg-transparent text-white rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+                    } ${todo.completed ? "line-through" : ""}`}
+                value={todoMsg}
+                onChange={(e) => setTodoMsg(e.target.value)}
+                readOnly={!isTodoEditable}
+            />
+            {/* Edit, Save Button */}
+            <button
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-[#F4A261] hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                onClick={() => {
+                    if (todo.completed) return;
+
+                    if (isTodoEditable) {
+                        editTodo();
+                    } else setIsTodoEditable((prev) => !prev);
+                }}
+                disabled={todo.completed}
+            >
+                {isTodoEditable ? <i className="fa-solid fa-floppy-disk text-white"></i> : <i className="fa-solid fa-pen-to-square text-white"></i>}
+            </button>
+            {/* Delete Todo Button */}
+            <button
+                className=" flex  align-middle w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center text-center bg-[#2A9D8F] hover:bg-gray-100 shrink-0"
+                onClick={() => deleteTodo(todo.id)}
+            >
+                <i className="text-white align-middle fa-regular fa-trash-can bg-[#2A9D8F]"></i>
+            </button>
+        </div>
+    );
+}
+
+export default TodoItem;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import ToDoForm from "./ToDoForm";
+import ToDoTask from "./ToDoTask";
+
+export default { ToDoForm, ToDoTask }
